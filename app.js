e7ff5087ce93b3d57e03a063a20750bb8d1192a4
H388kk <78017274+H388kk@users.noreply.github.com>
2021-03-09 15:45:35 +0200

    6vid

--- app.js
+++ app.js
@@ -13,13 +13,22 @@
 //var books = document.querySelector("#book-list li .name");
 //console.log(books);
 
-books = document.querySelectorAll("#book-list li .name");
+//books = document.querySelectorAll("#book-list li .name");
 //console.log(books);
 
-Array.from(books).forEach(function(book){
-    book.textContent += " (book title)";
-});
+//Array.from(books).forEach(function(book){
+//    book.textContent += " (book title)";
+//});
 
-const bookList = document.querySelector("#book-list");
+//const bookList = document.querySelector("#book-list");
 //bookList.innerHTML = "<h2>Books and more books...</h2>";
-bookList.innerHTML += "<p>This is how you add HTML</p>";
+//bookList.innerHTML += "<p>This is how you add HTML</p>";
+
+const banner = document.querySelector("#page-banner");
+
+console.log("page-banner node type is:", banner.nodeType);
+console.log("page-banner node name is:", banner.nodeName);
+console.log("page-banner has child nodes:", banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
