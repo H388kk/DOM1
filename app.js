c3ede36e537a653ba3b377144d68c270912211c2
H388kk <78017274+H388kk@users.noreply.github.com>
2021-03-09 15:33:01 +0200

    5vid

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-var titles = document.getElementsByClassName("title");
+//var titles = document.getElementsByClassName("title");
 
 //console.log(Array.isArray(titles));
 //console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
+//Array.from(titles).forEach(function(item){
+//    console.log(item);
+//})
 
-const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+//const wmf = document.querySelector("#book-list li:nth-child(2) .name");
 //console.log(wmf);
 
-var books = document.querySelector("#book-list li .name");
+//var books = document.querySelector("#book-list li .name");
 //console.log(books);
 
 books = document.querySelectorAll("#book-list li .name");
 //console.log(books);
 
 Array.from(books).forEach(function(book){
-    console.log(book);
+    book.textContent += " (book title)";
 });
 
+const bookList = document.querySelector("#book-list");
+//bookList.innerHTML = "<h2>Books and more books...</h2>";
+bookList.innerHTML += "<p>This is how you add HTML</p>";
