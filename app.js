1d46b3b05885f378bb644aa20e63df1240456d17
H388kk <78017274+H388kk@users.noreply.github.com>
2021-03-16 14:53:44 +0200

    10vid

--- app.js
+++ app.js
@@ -50,24 +50,35 @@
 
 //bookList.previousElementSibling.querySelector("p").innerHTML += "<br />Too cool for everyone else!";
 
-const listItems = document.querySelectorAll('#book-list ul li');
+//const listItems = document.querySelectorAll('#book-list ul li');
 
-Array.from(listItems).forEach(function(item){
-  item.addEventListener('click', (e) => {
+//Array.from(listItems).forEach(function(item){
+//  item.addEventListener('click', (e) => {
 
-    const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
-    li.parentNode.removeChild(li);
+//    const li = e.target.parentElement;
+//    console.log('child element to remove:', li);
+//    console.log('parent element to remove child from:', li.parentElement);
+//    li.parentNode.removeChild(li);
 
-  });
-});
+//  });
+//});
+
 
 // prevent default behaviour
 
-const link = document.querySelector('#page-banner a');
+//const link = document.querySelector('#page-banner a');
 
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
+//link.addEventListener('click', function(e){
+//  e.preventDefault();
+//  console.log('Navigation to', e.target.textContent, 'was prevented');
+//});
+
+const list = document.querySelector('#book-list ul');
+
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
 });
