6546c82b2c64b92d9b5718b607ed4d6d1ec8e4a1
H388kk <78017274+H388kk@users.noreply.github.com>
2021-03-16 15:18:37 +0200

    13Vid

--- app.js
+++ app.js
@@ -76,6 +76,66 @@
 const list = document.querySelector('#book-list ul');
 
 // delete books
+//list.addEventListener('click', (e) => {
+//  if(e.target.className == 'delete'){
+//    const li = e.target.parentElement;
+//    li.parentNode.removeChild(li);
+//  }
+//});
+
+//const forms = document.forms;
+//console.log(forms);
+//console.log(forms['add-book']);
+
+//Array.from(forms).forEach(function(form){
+//  console.log(form);
+//});
+
+//const addForm = forms['add-book'];
+//addForm.addEventListener('submit', function(e){
+//  e.preventDefault();
+//  const value = addForm.querySelector('input[type="text"]').value;
+//  console.log(value);
+//});
+
+//const list = document.querySelector('#book-list ul');
+//const forms = document.forms;
+
+// delete books
+//list.addEventListener('click', (e) => {
+//  if(e.target.className == 'delete'){
+//    const li = e.target.parentElement;
+//    li.parentNode.removeChild(li);
+//  }
+//});
+
+// add books
+//const addForm = forms['add-book'];
+//addForm.addEventListener('submit', function(e){
+//  e.preventDefault();
+
+  // create elements
+//  const value = addForm.querySelector('input[type="text"]').value;
+//  const li = document.createElement('li');
+//  const bookName = document.createElement('span');
+//  const deleteBtn = document.createElement('span');
+
+  // add text content
+//  bookName.textContent = value;
+//  deleteBtn.textContent = 'delete';
+
+  // append to DOM
+//  li.appendChild(bookName);
+//  li.appendChild(deleteBtn);
+//  list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
+//});
+/////
+
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
+
+// delete books
 list.addEventListener('click', (e) => {
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
@@ -83,17 +143,27 @@ list.addEventListener('click', (e) => {
   }
 });
 
-const forms = document.forms;
-console.log(forms);
-console.log(forms['add-book']);
-
-Array.from(forms).forEach(function(form){
-  console.log(form);
-});
-
+// add books
 const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
+
+  // create elements
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
